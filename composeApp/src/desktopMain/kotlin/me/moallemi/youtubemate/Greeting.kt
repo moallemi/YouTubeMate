d5f67089259cb10f94f3dd643b826ffd335c1f1f
Reza Moallemi <rm.allmails@gmail.com>
2024-08-21 20:14:25 -0700

    Init Repo

---
+++ composeApp/src/desktopMain/kotlin/me/moallemi/youtubemate/Greeting.kt
@@ -0,0 +1,9 @@
+package me.moallemi.youtubemate
+
+class Greeting {
+    private val platform = getPlatform()
+
+    fun greet(): String {
+        return "Hello, ${platform.name}!"
+    }
+}
---
+++ composeApp/src/desktopMain/kotlin/me/moallemi/youtubemate/Platform.kt
@@ -0,0 +1,7 @@
+package me.moallemi.youtubemate
+
+class JVMPlatform {
+    val name: String = "Java ${System.getProperty("java.version")}"
+}
+
+fun getPlatform() = JVMPlatform()
